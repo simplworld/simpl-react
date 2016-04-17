8f6d77e6b5409ae0c79c4e98916a9302080ad0ad
Flavio Curella <flavio.curella@gmail.com>
2016-04-17 16:11:41 -0500

    fix build

--- webpack.config.js
+++ webpack.config.js
@@ -50,4 +50,23 @@ var config = {
   }
 }
 
+if (process.env.NODE_ENV === 'production') {
+  config.plugins = [
+    new webpack.DefinePlugin({
+        'process.env.NODE_ENV': '"production"'
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+          warnings: false
+      }
+    })
+  ]
+} else {
+  config.plugins = [
+    new webpack.DefinePlugin({
+        'process.env.NODE_ENV': '"development"'
+    })
+  ]
+}
 module.exports = config;
