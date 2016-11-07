2324879bcdf0cd9733c1b856d8f81671930677ef
Flavio Curella <flavio.curella@gmail.com>
2016-11-07 10:25:43 -0600

    add api docs to validators

--- src/components/forms/validators.js
+++ src/components/forms/validators.js
@@ -1,3 +1,10 @@
+/**
+ * Check if the provided values is smaller than the `min` prop.
+ *
+ * @param      {string}  value     The value to validate
+ * @param      {object}  ownProps  The component's own properties
+ * @return     {string|undefined}  Either an error message, or `undefined`
+ */
 export const min = (value, ownProps) => {
   if (parseFloat(value) < ownProps.min) {
     return `Value can't be less than ${ownProps.min}.`;
@@ -5,6 +12,13 @@ export const min = (value, ownProps) => {
   return undefined;
 };
 
+/**
+ * Check if the provided values is greater than the `max` prop.
+ *
+ * @param      {string}  value     The value to validate
+ * @param      {object}  ownProps  The component's own properties
+ * @return     {string|undefined}  Either an error message, or `undefined`
+ */
 export const max = (value, ownProps) => {
   if (parseFloat(value) > ownProps.max) {
     return `Value can't be greater than ${ownProps.max}.`;
