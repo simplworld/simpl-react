3919bebe1782701a2b67ac40b67c69cb3ea915a6
David Leong <david0leong@gmail.com>
2019-02-21 07:30:38 -0800

    Remove redux-router from codebase as it is not relevant

--- src/stores.js
+++ src/stores.js
@@ -3,13 +3,10 @@
  * @memberof Simpl
  */
 import { createStore, applyMiddleware, compose } from 'redux';
-import { reduxReactRouter } from 'redux-router';
 
 import promiseMiddleware from 'redux-promise';
 import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
-
-import createHistory from 'history/lib/createBrowserHistory';
 
 const loggerMiddleware = createLogger();
 
@@ -19,7 +16,6 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const defaultMiddlewares = [
   applyMiddleware(promiseMiddleware),
   applyMiddleware(thunkMiddleware),
-  reduxReactRouter({ createHistory }),
 ];
 
 /**
